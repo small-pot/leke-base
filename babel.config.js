4095177ac4687fda85b4351e6e390707d4e4004a
guoquankai <719951601@qq.com>
2020-11-12 17:44:19 +0800

    更新下babel-jest配置

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
+const path=require('path');
 module.exports = {
     presets: [
         ["@babel/preset-env",{targets:{node:'current'}}],
         "@babel/preset-typescript",
         "@babel/preset-react"
+    ],
+    plugins: [
+        path.resolve(__dirname,'./packages/ssr/build/babel/plugin-node-style.js')
     ]
 };
