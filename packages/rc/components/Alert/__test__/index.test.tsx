9224fbe955b6b087f1f2c39c7570724bb2d15a82
李国帝 <liguodi@cnstrong.cn>
2020-12-11 15:40:24 +0800

    feat: 测试用例跑到80%以上

--- packages/rc/components/Alert/__test__/index.test.tsx
+++ packages/rc/components/Alert/__test__/index.test.tsx
@@ -4,14 +4,6 @@ import '@testing-library/jest-dom/extend-expect';
 import {render, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-const delayFn = async (time) => {
-    return new Promise((reslove) => {
-        setTimeout(() => {
-            reslove();
-        }, time);
-    });
-};
-
 describe('Alert ', function() {
     it('test default',async function () {
         const {container} = render(
@@ -20,25 +12,90 @@ describe('Alert ', function() {
             />
         );
         const wrap = container.querySelector('.leke-alert-container');
-        const message = wrap.getElementsByClassName('leke-alert-message')[0].textContent;
-        expect(message).toContain('hello world');
+        expect(wrap).toContainHTML('hello world');
     });
 
-    // it('could be closed',async function () {
-    //     const {container} = render(
-    //         <Alert
-    //             className={"my-test-alert"}
-    //             message={"hello world"}
-    //             isShowCloseIcon
-    //             type={"success"}
-    //         />
-    //     );
-    //     const closeEle = container.getElementsByClassName('leke-alert-close-box')[0];
-    //     userEvent.click(closeEle);
-    //     const findWrap1 = document.getElementsByClassName('my-test-alert');
-    //     expect(findWrap1.length).toBe(1);
-    //     await delayFn(500);
-    //     const findWrap2 = document.getElementsByClassName('my-test-alert');
-    //     expect(findWrap2.length).toBe(0);
-    // });
+    it('test be closed',async function () {
+        const afterClose= jest.fn();
+        const {container} = render(
+            <Alert
+                className={"my-test-alert"}
+                message={"hello world"}
+                afterClose={afterClose}
+            />
+        );
+        const wrap = container.querySelector('.my-test-alert');
+        expect(wrap).toContainHTML('hello world');
+        const closeEle = document.querySelector('.leke-alert-close-box');
+        userEvent.click(closeEle);
+        waitFor(() => {
+            console.log('123123');
+            const findWrap = container.querySelector('.my-test-alert');
+            expect(findWrap).toBeEmptyDOMElement();
+            expect(afterClose).toHaveBeenCalled();
+        }, {
+            timeout: 500
+        });
+    });
+
+    it('test success type',async function () {
+        const {container} = render(
+            <Alert
+                className={"my-test-alert"}
+                message={"hello world"}
+                type={'success'}
+            />
+        );
+        const wrap = container.querySelector('.my-test-alert');
+        expect(wrap).toHaveClass('leke-alert-container-success');
+    });
+
+    it('test title message',async function () {
+        const {container} = render(
+            <Alert
+                className={"my-test-alert"}
+                message={"这是一段message"}
+                type={'success'}
+                title={'这是一个标题'}
+            />
+        );
+        const message = container.querySelector('.leke-alert-message');
+        expect(message).toContainHTML('这是一段message');
+        const title = container.querySelector('.leke-alert-title');
+        expect(title).toContainHTML('这是一个标题');
+    });
+
+    it('test icon hide closeIcon hide',async function () {
+        const {container} = render(
+            <Alert
+                className={"my-test-alert"}
+                message={"这是一段message"}
+                type={'success'}
+                title={'这是一个标题'}
+                icon={null}
+                closeIcon={null}
+            />
+        );
+        const closeIcon = container.querySelector('.leke-alert-close-box');
+        expect(closeIcon).toEqual(null);
+        const icon = container.querySelector('.leke-alert-icon-box');
+        expect(icon).toEqual(null);
+    });
+
+    it('test other props',async function () {
+
+        const {container} = render(
+            <Alert
+                className={"my-test-alert"}
+                action={
+                    <button className="action-button">1</button>
+                }
+                style={{color: 'red'}}
+            />
+        );
+        const wrap = container.querySelector('.my-test-alert');
+        expect(wrap).toHaveStyle('color: red');
+        const actionButton = container.querySelector('.action-button');
+        expect(actionButton).not.toBeEmptyDOMElement();
+    });
 });
