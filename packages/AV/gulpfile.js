316695164eb0fa837dfd92fb1ac2823ff6330237
guoquankai <719951601@qq.com>
2021-01-18 17:13:43 +0800

    增加rollup打包

--- packages/AV/gulpfile.js
+++ packages/AV/gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
-const {buildTs,buildLess}=require('@leke/gulp-compile');
+const {buildTs}=require('@leke/gulp-compile');
+const rename=require('gulp-rename')
+const gulpLess=require('gulp-less')
 const tsEntry=['src/!(demos|__test__)/*.ts','src/index.ts'];
+const lessEntry=['src/**/*.less']
 exports.default = gulp.parallel(
     ()=>buildTs({stream:gulp.src(tsEntry),outDir:'es',modules:false}),
     ()=>buildTs({stream:gulp.src(tsEntry),outDir:'lib',modules:'commonjs'}),
-    ()=>buildLess({stream:gulp.src(['src/**/*.less']),outDir:['es','lib']}),
+    ()=>gulp.src(lessEntry).pipe(gulp.dest("style")).pipe(gulpLess()).pipe(gulp.dest("style")),
+    ()=>gulp.src('src/AudioPlayer/index.less').pipe(gulpLess()).pipe(rename('AudioPlayer.css')).pipe(gulp.dest("./dist")),
+    ()=>gulp.src('src/VideoPlayer/index.less').pipe(gulpLess()).pipe(rename('VideoPlayer.css')).pipe(gulp.dest("./dist"))
 );
