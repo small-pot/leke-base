3eb65c4bc70aa8bf5b799b8da21b5f1d4253ebee
guoquankai <719951601@qq.com>
2021-01-21 14:03:41 +0800

    更新

--- packages/rc/components/AudioPlayer/index.tsx
+++ packages/rc/components/AudioPlayer/index.tsx
@@ -17,10 +17,11 @@ interface AudioPlayerProps extends Partial<AudioPlayerNativeEvent> {
     loop?:boolean,
     allowSeek?:boolean,
     preload?:'none' | 'metadata' | 'auto' | '',
+    timeFormat?:(num:number)=>string
 }
 
 const AudioPlayer = (props:AudioPlayerProps,ref) => {
-    const {className,style,src='',autoplay=false,paused,loop=false,preload='metadata',allowSeek=true,timeFormat} = props;
+    const {className,style,src='',autoplay=false,paused,loop=false,preload='metadata',allowSeek=true,timeFormat,...eventProps} = props;
     const audioRef = React.useRef<AudioPlayerCls>(null);
     const $audioContainer = React.useRef<HTMLDivElement>(null);
     React.useEffect(()=>{
@@ -51,25 +52,14 @@ const AudioPlayer = (props:AudioPlayerProps,ref) => {
             allowSeek,
             allowPlayControl:!(paused!==null && paused!==undefined),
         });
-        audioPlayer.on('audioprocess',props.onAudioProcess || noop);
-        audioPlayer.on('canplay',props.onCanplay || noop);
-        audioPlayer.on('canplaythrough',props.onCanplayThrough || noop);
-        audioPlayer.on('durationchange',props.onDurationChange || noop);
-        audioPlayer.on('emptied',props.onEmptied || noop);
-        audioPlayer.on('ended',props.onEnded || noop);
-        audioPlayer.on('loadeddata',props.onLoadedData || noop);
-        audioPlayer.on('loadedmetadata',props.onLoadedMetaData || noop);
-        audioPlayer.on('pause',props.onPause || noop);
-        audioPlayer.on('play',props.onPlay || noop);
-        audioPlayer.on('playing',props.onPlaying || noop);
-        audioPlayer.on('ratechange',props.onRateChange || noop);
-        audioPlayer.on('seeked',props.onSeeked || noop);
-        audioPlayer.on('seeking',props.onSeeking || noop);
-        audioPlayer.on('stalled',props.onStalled || noop);
-        audioPlayer.on('suspend',props.onSuspend || noop);
-        audioPlayer.on('timeupdate',props.onTimeUpdate || noop);
-        audioPlayer.on('volumechange',props.onVolumeChange || noop);
-        audioPlayer.on('waiting',props.onWaiting || noop);
+        Object.keys(eventProps).forEach((key)=>{
+            if(/^on/.test(key)){
+                const eventName=key.replace(/^on([A-Z])/,($0,$1)=>{
+                    return $1.toLowerCase()
+                })
+                audioPlayer.on(eventName,eventProps[key] || noop);
+            }
+        })
         if (ref) {
             ref.current = audioPlayer;
         }
