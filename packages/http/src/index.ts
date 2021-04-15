3a6a82303de686acdfd619a55600ae8c6e3bc60b
guoquankai <719951601@qq.com>
2021-04-15 10:50:55 +0800

    http 版本升级

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios,{AxiosRequestConfig} from "axios";
 import createHttp from "./create";
-export * from './types';
+export * from "./types";
 
+export type httpRequest=AxiosRequestConfig;
 const cancelToken = axios.CancelToken;
 
 const http=createHttp();
--- packages/http/src/types.ts
+++ packages/http/src/types.ts
@@ -1,9 +1,29 @@
-import {AxiosRequestConfig, AxiosResponse} from "axios";
+import {AxiosRequestConfig, AxiosResponse, AxiosInterceptorManager} from 'axios';
 
-export interface httpRequest extends AxiosRequestConfig{
-    reset?:boolean,
-}
-export interface createOption extends AxiosRequestConfig{
+export type httpRequest=AxiosRequestConfig;
+
+export interface createOption extends httpRequest{
     requestInterceptor?:(config:httpRequest)=>httpRequest,
     responseInterceptor?:<T=any>(res:AxiosResponse)=>T
+}
+
+export type httpPromise<T=any>=Promise<T>
+
+export interface httpInstance {
+    (config: httpRequest): httpPromise;
+    (url: string, config?: AxiosRequestConfig): httpPromise;
+    defaults: httpRequest;
+    interceptors: {
+        request: AxiosInterceptorManager<httpRequest>;
+        response: AxiosInterceptorManager<httpRequest>;
+    };
+    getUri(config?: httpRequest): string;
+    request<T = any> (config: httpRequest): Promise<T>;
+    get<T = any>(url: string, config?: httpRequest): Promise<T>;
+    delete<T = any>(url: string, config?: httpRequest): Promise<T>;
+    head<T = any>(url: string, config?: httpRequest): Promise<T>;
+    options<T = any>(url: string, config?: httpRequest): Promise<T>;
+    post<T = any>(url: string, data?: any, config?: httpRequest): Promise<T>;
+    put<T = any>(url: string, data?: any, config?: httpRequest): Promise<T>;
+    patch<T = any>(url: string, data?: any, config?: httpRequest): Promise<T>;
 }
