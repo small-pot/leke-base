3044fa106280449b3f7b73c9d23c64c008ccd91b
lurui <lurui@cnstrong.cn>
2021-05-10 14:25:48 +0800

    feat: 弹出动画增加偏移量

--- packages/rc/components/Tooltip/util.ts
+++ packages/rc/components/Tooltip/util.ts
@@ -15,6 +15,8 @@ type placementType =
 export function setPopupPosition(popup:HTMLElement, trigger:HTMLElement, placement:placementType) {
     const top = popup.offsetTop;
     const left = popup.offsetLeft;
+    const height = popup.offsetHeight;
+    const width = popup.offsetWidth;
     const transverse = 24;  //左右偏移量
     const portrait = 16;    //上下偏移量
     const triggerWidth = trigger.offsetWidth;
@@ -24,27 +26,35 @@ export function setPopupPosition(popup:HTMLElement, trigger:HTMLElement, placeme
     switch (placement) {
     case "topLeft":
         popup.style.left = left + topAndBottom + 'px';
+        popup.parentElement.style.transformOrigin = left + topAndBottom + 'px bottom';
         break;
     case "topRight":
         popup.style.left = left - topAndBottom + 'px';
+        popup.parentElement.style.transformOrigin = left - topAndBottom + width + 'px bottom';
         break;
     case "bottomLeft":
         popup.style.left = left + topAndBottom + 'px';
+        popup.parentElement.style.transformOrigin = left + topAndBottom + 'px top';
         break;
     case "bottomRight":
         popup.style.left = left - topAndBottom + 'px';
+        popup.parentElement.style.transformOrigin =  left - topAndBottom + width + 'px top';
         break;
     case "leftTop":
         popup.style.top = top + leftAndRight + 'px';
+        popup.parentElement.style.transformOrigin = 'right ' + top + leftAndRight + 'px';
         break;
     case "leftBottom":
         popup.style.top = top - leftAndRight + 'px';
+        popup.parentElement.style.transformOrigin = 'right ' + (top - leftAndRight + height) + 'px';
         break;
     case "rightTop":
         popup.style.top = top + leftAndRight + 'px';
+        popup.parentElement.style.transformOrigin = 'left ' + top + leftAndRight + 'px';
         break;
     case "rightBottom":
         popup.style.top = top - leftAndRight + 'px';
+        popup.parentElement.style.transformOrigin = 'left ' + (top - leftAndRight + height) + 'px';
         break;
     }
 }
