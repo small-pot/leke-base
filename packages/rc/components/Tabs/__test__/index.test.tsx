ea4e3d15db5d7eff810810cf928c61eac24cdb25
zhoujd <810967128@qq.com>
2021-05-28 16:16:39 +0800

    update: 补充单元测试

--- packages/rc/components/Tabs/__test__/index.test.tsx
+++ packages/rc/components/Tabs/__test__/index.test.tsx
@@ -15,20 +15,24 @@ window.resizeTo = function resizeTo(width, height) {
     }).dispatchEvent(new this.Event('resize'));
 };
 
-const originalOffsetHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetHeight');
-const originalOffsetWidth = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetWidth');
+// const originalOffsetHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetHeight');
+// const originalOffsetWidth = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetWidth');
 
-beforeAll(() => {
-    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', { configurable: true, value: 1080 });
-    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', { configurable: true, value: 400 });
-});
+// beforeAll(() => {
+//     Object.defineProperty(HTMLElement.prototype, 'offsetHeight', { configurable: true, value: 1080 });
+//     Object.defineProperty(HTMLElement.prototype, 'offsetWidth', { configurable: true, value: 400 });
+// });
 
-afterAll(() => {
-    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', originalOffsetHeight);
-    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', originalOffsetWidth);
-});
-afterEach(cleanup);
-jest.useFakeTimers();
+// afterAll(() => {
+//     Object.defineProperty(HTMLElement.prototype, 'offsetHeight', originalOffsetHeight);
+//     Object.defineProperty(HTMLElement.prototype, 'offsetWidth', originalOffsetWidth);
+// });
+// afterEach(cleanup);
+// jest.useFakeTimers();
+
+
+/*FIXME: test-library无法检测元素大小，导致滚动相关代码都无法走到*/
+
 describe('Rate', function() {
     it('test basis',async function () {
         const onChange = jest.fn();
@@ -44,7 +48,6 @@ describe('Rate', function() {
                 <Tabs.TabPane tab="Tab 3" key="3">
                     Content of Tab Pane 3
                 </Tabs.TabPane>
-                <div>22312312312</div>
             </Tabs>
         );
         const tab1 = container.querySelectorAll('.leke-tabs-nav-item')[0];
@@ -58,31 +61,23 @@ describe('Rate', function() {
         expect(tab1).not.toHaveClass('leke-tabs-nav-active',{ exact: false });
         expect(tab2).toHaveClass('leke-tabs-nav-active',{ exact: false });
     });
-    it('test scroll',async function () {
-        
+    it('test edit',async function () {
+        const onEdit = jest.fn();
         const {container} = render(
-            <div style={{ width: '100px', overflow: 'hidden' }}>
-                <Tabs type="card" defaultActiveKey="2">
-                    {Array(30).fill('').map((_,i) => (
-                        <Tabs.TabPane tab={`Tab ${i + 1}`} key={i+1} disabled={i===20}>
+            <Tabs type="editable-card" defaultActiveKey="2" onEdit={onEdit}>
+                {Array(3).fill('').map((_,i) => (
+                    <Tabs.TabPane tab={`Tab ${i + 1}`} key={i+1} disabled={i===20}>
                         Content of Tab Pane {i + 1}
-                        </Tabs.TabPane>
-                    ))}
-                </Tabs>
-            </div>
+                    </Tabs.TabPane>
+                ))}
+            </Tabs>
         );
-
-        // screen.debug();
-        // const listWrap = container.querySelector('.leke-tabs-nav');
-        // console.log('clientWidth  =======================>',listWrap.clientWidth);
-        // act(() => {
-        //     window.resizeTo(500, 500);
-        // });
-        
-        // const moreBtn = container.querySelector('.leke-tabs-more-btn');
-        // expect(moreBtn).toBeInTheDocument();
-
-        
+        const addBtn = container.querySelector('.leke-tabs-add-button');
+        userEvent.click(addBtn);
+        expect(onEdit).toBeCalled();
+        userEvent.hover(container.querySelector('.leke-tabs-nav-list'));
+        userEvent.unhover(container.querySelector('.leke-tabs-nav-list'));
+        window.resizeTo(500,500);
     });
 });
 
